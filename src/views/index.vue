62638b5a60aa231bed7bee7f2ea476f679d7d077
Verazh <31238774+verazhoo@users.noreply.github.com>
2023-05-23 22:55:17 +0800

    修改自自适应

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,100 +1,110 @@
 <template>
     <a-layout class="abyss">
-        <a-layout-sider class="sider" :width="200">
-            <!-- <a-row :xs="0" :sm="0" :md="24" :lg="12" :xl="6"> -->
-            <div class="logo"></div>
-            <a-menu v-model:selectedKeys="selected_name" v-model:openKeys="open_keys" mode="inline">
-                <template v-for="item in abyss.stack" :key="item.name">
-                    <a-menu-item
-                        v-if="!item.children"
-                        :key="item.name"
-                        @click="handleMenu(item.name, item.name)"
-                    >{{item.name}}</a-menu-item>
+        <a-row type="flex" justify="space-around">
+            <a-col flex="200px">
+                <a-layout-sider class="sider" :width="200">
+                    <div class="logo"></div>
+                    <a-menu
+                        v-model:selectedKeys="selected_name"
+                        v-model:openKeys="open_keys"
+                        mode="inline"
+                    >
+                        <template v-for="item in abyss.stack" :key="item.name">
+                            <a-menu-item
+                                v-if="!item.children"
+                                :key="item.name"
+                                @click="handleMenu(item.name, item.name)"
+                            >{{item.name}}</a-menu-item>
 
-                    <a-sub-menu v-if="item.children" :key="item.name">
-                        <template #title>{{item.name}}</template>
-                        <a-menu-item
-                            v-for="sub_item in item.children"
-                            :key="sub_item.name"
-                            @click="handleMenu(sub_item.name, item.name)"
-                        >{{sub_item.name}}</a-menu-item>
-                    </a-sub-menu>
-                </template>
-            </a-menu>
-            <!-- </a-row> -->
-        </a-layout-sider>
-        <a-layout class="contain">
-            <a-layout-header class="header"></a-layout-header>
-            <a-layout-content class="content">
-                <template v-for="item in abyss.stack" :key="item.name">
-                    <template v-if="!item.children && item.web">
-                        <a-typography class="site">
-                            <a-typography-title
-                                class="title"
-                                :id="item.name"
-                                :level="3"
-                            >{{item.name}}</a-typography-title>
-                            <a-row class="item-row">
-                                <a-col
-                                    class="item-col"
-                                    v-for="site in item.web"
-                                    :key="site.title"
-                                    :xs="24"
-                                    :sm="24"
-                                    :md="24"
-                                    :lg="12"
-                                    :xl="6"
-                                >
-                                    <a-card hoverable @click="handleHerf(site.url)">
-                                        <a-card-meta :title="site.title" :description="site.desc">
-                                            <template #avatar>
-                                                <a-avatar :src="getLogoUrl(site.logo)" />
-                                            </template>
-                                        </a-card-meta>
-                                    </a-card>
-                                </a-col>
-                            </a-row>
-                        </a-typography>
-                    </template>
-
-                    <template v-if="item.children">
-                        <template v-for="sub_item in item.children" :key="sub_item.name">
-                            <a-typography class="site" v-if="sub_item.web">
-                                <a-typography-title
-                                    class="title"
-                                    :id="sub_item.name"
-                                    :level="3"
-                                >{{sub_item.name}}</a-typography-title>
-                                <a-row class="item-row">
-                                    <a-col
-                                        class="item-col"
-                                        v-for="site in sub_item.web"
-                                        :key="site.title"
-                                        :xs="24"
-                                        :sm="24"
-                                        :md="24"
-                                        :lg="12"
-                                        :xl="6"
-                                    >
-                                        <a-card hoverable @click="handleHerf(site.url)">
-                                            <a-card-meta
-                                                :title="site.title"
-                                                :description="site.desc"
-                                            >
-                                                <template #avatar>
-                                                    <a-avatar :src="getLogoUrl(site.logo)" />
-                                                </template>
-                                            </a-card-meta>
-                                        </a-card>
-                                    </a-col>
-                                </a-row>
-                            </a-typography>
+                            <a-sub-menu v-if="item.children" :key="item.name">
+                                <template #title>{{item.name}}</template>
+                                <a-menu-item
+                                    v-for="sub_item in item.children"
+                                    :key="sub_item.name"
+                                    @click="handleMenu(sub_item.name, item.name)"
+                                >{{sub_item.name}}</a-menu-item>
+                            </a-sub-menu>
                         </template>
-                    </template>
-                </template>
-            </a-layout-content>
-        </a-layout>
+                    </a-menu>
+                </a-layout-sider>
+            </a-col>
+            <a-col flex="1">
+                <a-layout class="contain">
+                    <a-layout-header class="header"></a-layout-header>
+                    <a-layout-content class="content">
+                        <template v-for="item in abyss.stack" :key="item.name">
+                            <template v-if="!item.children && item.web">
+                                <a-typography class="site">
+                                    <a-typography-title
+                                        class="title"
+                                        :id="item.name"
+                                        :level="3"
+                                    >{{item.name}}</a-typography-title>
+                                    <a-row class="item-row">
+                                        <a-col
+                                            class="item-col"
+                                            v-for="site in item.web"
+                                            :key="site.title"
+                                            :xs="24"
+                                            :sm="24"
+                                            :md="12"
+                                            :lg="12"
+                                            :xl="6"
+                                        >
+                                            <a-card hoverable @click="handleHerf(site.url)">
+                                                <a-card-meta
+                                                    :title="site.title"
+                                                    :description="site.desc"
+                                                >
+                                                    <template #avatar>
+                                                        <a-avatar :src="getLogoUrl(site.logo)" />
+                                                    </template>
+                                                </a-card-meta>
+                                            </a-card>
+                                        </a-col>
+                                    </a-row>
+                                </a-typography>
+                            </template>
 
+                            <template v-if="item.children">
+                                <template v-for="sub_item in item.children" :key="sub_item.name">
+                                    <a-typography class="site" v-if="sub_item.web">
+                                        <a-typography-title
+                                            class="title"
+                                            :id="sub_item.name"
+                                            :level="3"
+                                        >{{sub_item.name}}</a-typography-title>
+                                        <a-row class="item-row">
+                                            <a-col
+                                                class="item-col"
+                                                v-for="site in sub_item.web"
+                                                :key="site.title"
+                                                :xs="24"
+                                                :sm="24"
+                                                :md="12"
+                                                :lg="12"
+                                                :xl="6"
+                                            >
+                                                <a-card hoverable @click="handleHerf(site.url)">
+                                                    <a-card-meta
+                                                        :title="site.title"
+                                                        :description="site.desc"
+                                                    >
+                                                        <template #avatar>
+                                                            <a-avatar :src="getLogoUrl(site.logo)" />
+                                                        </template>
+                                                    </a-card-meta>
+                                                </a-card>
+                                            </a-col>
+                                        </a-row>
+                                    </a-typography>
+                                </template>
+                            </template>
+                        </template>
+                    </a-layout-content>
+                </a-layout>
+            </a-col>
+        </a-row>
         <a-back-top />
     </a-layout>
 </template>
@@ -159,7 +169,6 @@ const handleHerf = (url) => {
     }
 
     .contain {
-        margin-left: 200px;
         .header {
             position: fixed;
             z-index: 1;
